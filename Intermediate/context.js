d133c767a7f957ed64e357cd9b3fa4cc16f1577e
Aena <aenakhichi62108@gmail.com>
2024-05-21 16:13:24 +0530

    context

---
+++ Intermediate/context.js
@@ -0,0 +1,14 @@
+// sayHello();
+
+// function sayHello() {
+//     console.log("Hello");
+// }
+
+if (2 === 2){
+    console.log("this is true");
+}
+
+var myName = "Aena";
+if (myName === myName){
+    console.log("This is again a true statement");
+}
