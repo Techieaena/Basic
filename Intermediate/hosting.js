7dd63198d7eb6eb8bd6e07b635308e26ce4b75ee
Aena <aenakhichi62108@gmail.com>
2024-05-21 17:17:04 +0530

    hosting

--- Intermediate/hosting.js
+++ Intermediate/hosting.js
@@ -1,6 +1,13 @@
+tipper("5");
+
 function tipper(a){
 var  bill = parseInt(a);
 console.log(bill + 5);
 }
 
-tipper("5");
+var bigTipper = function (a) {
+    var bill = parseInt(a);
+    console.log(bill + 15);
+};
+
+bigTipper("200");
